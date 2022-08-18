4d4ff6152a775c00c86c04d4ccfaf5790024b395
한비 <hanqi1417@gmail.com>
2022-08-18 16:47:43 +0900

    20220818

---
+++ portfoilo(양게임)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public AudioClip deathClip;
+    public float jumpForce = 100f;
+
+    private int jumpCount = 0;
+    private bool isGrounded = false;
+    private bool isDead = false;
+
+    private Rigidbody2D playerRigibody;
+    private Animator animator;
+    private AudioSource playerAudio;
+
+
+    private void Start()
+    {
+        playerRigibody = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (isDead)
+        {
+            return;
+        }
+        if (Input.GetMouseButtonDown(0) && jumpForce < 2)
+        {
+            jumpCount++;
+            playerRigibody.velocity = Vector2.zero;
+            playerRigibody.AddForce(new Vector2(0, jumpCount));
+            playerAudio.Play(); 
+        }
+
+        animator.SetBool("Grounded", isGrounded);
+    }
+    private void Die()
+    {
+        animator.SetTrigger("Die");
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+}
